cd7b890273b51d467cb1078cdab835687ee82021
“anstarky” <anstarky@gmail.com>
2020-01-17 21:32:31 +0200

    Updates

---
+++ precache-manifest.724c0e2c2cd5f84b3ef5aed57f5955c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'ca73741e7c318d20b94d7036c42feaab',
+    url: '/goit-react-hw-04-movies/index.html',
+  },
+  {
+    revision: 'dd5aa1120984e2281981',
+    url: '/goit-react-hw-04-movies/static/css/2.764ccc25.chunk.css',
+  },
+  {
+    revision: '68bc0c99f1378fc76188',
+    url: '/goit-react-hw-04-movies/static/css/main.dd6179c5.chunk.css',
+  },
+  {
+    revision: 'dd5aa1120984e2281981',
+    url: '/goit-react-hw-04-movies/static/js/2.469914b0.chunk.js',
+  },
+  {
+    revision: '68bc0c99f1378fc76188',
+    url: '/goit-react-hw-04-movies/static/js/main.fe7cf55a.chunk.js',
+  },
+  {
+    revision: 'a7cd2099343d2842d2c4',
+    url: '/goit-react-hw-04-movies/static/js/runtime-main.2c61838f.js',
+  },
+]);
